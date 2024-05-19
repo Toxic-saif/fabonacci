8d797d2c984cca0bd4e66bfc260c78657569aa20
Toxic-saif <111408565+Toxic-saif@users.noreply.github.com>
2024-05-19 16:54:05 +0530

    Add files via upload

---
+++ Matrix_determinant.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+void main()
+{
+    int i,j;
+    int a[3][3],b[3][3],c[3][3];
+    printf("Enter the elements of matix(A): ");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("Enter the elements of matrix(B): ");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            c[i][j]=a[i][j]+b[i][j];
+        }
+    }
+    printf("sum of matrix(A) and matrix(B): \n");
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            printf("%d ",c[i][j]);
+        }
+        printf("\n");
+    }
+}
